f955e24a8ffc8594e59a35fab44d209e861ed972
Gouravsingh@7 <120244528+GOURAVSINGH19@users.noreply.github.com>
2024-07-05 07:55:56 +0000

    Navbar

--- src/component/Vscode.jsx
+++ src/component/Vscode.jsx
@@ -1,17 +1,31 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Editor } from "@monaco-editor/react";
+import Navbar from "./Navbar";
+
+const navItems = [
+  {
+    name: "undefined",
+    id: 1,
+  },
+  {
+    name: "html",
+    id: 2,
+  },
+  {
+    name: "undefined",
+    id: 3,
+  },
+];
 
 const vscode = () => {
   const [isopen, setisopen] = useState(true);
+  const [navitems,setnavitems] =useState(navItems)
+
   const [code, setcode] = useState({
     html: "<!-- write your HTML code here -->\n<div>output</div>",
     css: "/* write your CSS code here */ \n div {\n color: white \n}\n",
     js: "// write your JS code here",
   });
-
-  const handlerclickeditor = (e) => {
-    setisopen(!isopen);
-  }
 
   const src = `
   <!DOCTYPE html>
@@ -26,68 +40,60 @@ const vscode = () => {
 </html>
   `;
 
-  return (
-    <div className=" w-full  text-white ">
-      <div className="flex gap-1 p-5">
-        <div className="h-full">
-          <button onClick={handlerclickeditor}  className=" bg-zinc-700  border-none outline-none px-3 py-1 mb-1">
-            Html
-          </button>
-         {isopen && <Editor
-            width="32vw"
-            height="50vh"
-            language="html"
-            defaultValue={code.html}
-            theme="vs-dark"
-            loading="Thanks for wait"
-            onChange={(newValue, e) => {
-              setcode({ ...code, html: newValue });
-            }}
-          />}
 
+  const handleclick = (id) => {
+    setnavitems(navitems.filter((itemsid) => itemsid.id !== id))
+  };
+
+  return (
+    <div>
+      <div className=" w-screen h-screen text-white flex  overflow-hidden ">
+        <div className="w-[15%]  bg-zinc-800 border-r-[1px] border-zinc-600">
+          <Navbar />
         </div>
-        <div>
-          <button onClick={handlerclickeditor} className=" bg-zinc-700  border-none outline-none px-3 py-1 mb-1">
-            Css
-          </button>
-         {isopen && <Editor
-            width="32vw"
-            height="50vh"
-            language="css"
-            defaultValue={code.css}
-            theme="vs-dark"
-            loading="Thanks for wait"
-            onChange={(newcode, e) => {
-              setcode({ ...code, css: newcode });
-            }}
-          />}
+        <div className="bg-black-50 w-[70%] flex flex-col justify-start items-start relative">
+          <div className="flex py-2 px-3">
+            {navitems.map((items) => (
+              <div
+                key={items.id}
+                className="flex items-center  border-r-2 border-zinc-600 bg-zinc-800"
+                
+              >
+                <h1 className="border-none outline-none px-3 py-1">
+                  {items.name}
+                </h1>
+                <span
+                  className="w-10 text-[14px] cursor-pointer"
+                  onClick={() => handleclick(items.id)}
+                >
+                  ❌
+                </span>
+              </div>
+            ))}
+          </div>
+          {isopen && (
+            <Editor
+              width="70vw"
+              height="100vh"
+              language="html"
+              defaultValue={code.html}
+              theme="vs-dark"
+              loading="Thanks for wait"
+              onChange={(newValue) => {
+                setcode({ ...code, html: newValue });
+              }}
+            />
+          )}
         </div>
-        <div>
-          <button onClick={handlerclickeditor} className=" bg-zinc-700  border-none outline-none px-3 py-1 mb-1">
-            Javascript
-          </button>
-         { isopen && <Editor
-            width="32vw"
-            height="50vh"
-            language="javascript"
-            defaultValue={code.js}
-            theme="vs-dark"
-            loading="Thanks for wait"
-            onChange={(newcode, e) => {
-              setcode({ ...code, js: newcode });
-            }}
-          />}
+        <div className="flex-1 w-[10vw] h-[100vh] bg-zinc-700 py-2  text-white">
+          <h1 className="text-gray-400">//output</h1>
+          <iframe
+            className="flex-1 w-full h-full text-white"
+            aria-label="Preview"
+            sandbox="allow-scripts"
+            srcDoc={src}
+          ></iframe>
         </div>
-      </div>
-      <div className="flex-1 max-w-screen-md w-auto h-[30vh] bg-zinc-700 p-5 m-5 text-white">
-        <h1 className="text-gray-400">//output</h1>
-        {/* <div dangerouslySetInnerHTML={{__html: code.html}}></div> */}
-        <iframe
-          className="flex-1 w-full h-full text-white"
-          aria-label="Preview"
-          sandbox="allow-scripts"
-          srcDoc={src}
-        ></iframe>
       </div>
     </div>
   );
